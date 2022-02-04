e968c012cc79313fc2aa8667c44cf76d9b8ae213
Sagar Jha <jsagar1846@gmail.com>
2022-02-05 01:56:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Graph from "./Component/Graph";
+import Navbar from "./Component/Navbar";
+import Pagination from "./Component/Pagination";
+import Table from "./Component/Table";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  //pagination
+  const [postPerPage] = useState(25);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch("http://fetest.pangeatech.net/data")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+    return () => {};
+  }, []);
+
+  console.log(filteredData);
+
+  //page
+
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
+
+  //page change
+
+  const pageChange = (el) => setCurrentPage(el);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar
+        data={data}
+        filteredData={filteredData}
+        setFilteredData={setFilteredData}
+      />
+      <div className="container">
+        <Graph data={data} filteredData={filteredData} />
+        <Table data={currentPost} filteredData={filteredData} />
+        {filteredData.length === 0 ? (
+          <Pagination
+            postPerPage={postPerPage}
+            totalPost={data.length}
+            pageChange={pageChange}
+          />
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Component/Graph.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
+function Graph({ data, filteredData }) {
+  let acv, month, tcv;
+  if (filteredData.length === 0) {
+    acv = data.map((el) => el.acv);
+    month = data.map((el) => el.month);
+    tcv = data.map((el) => el.tcv);
+  } else {
+    acv = filteredData.map((el) => el.acv);
+    tcv = filteredData.map((el) => el.tcv);
+    month = filteredData.map((el) => el.month);
+  }
+
+  console.log(acv, month);
+  return (
+    <div>
+      <h1>Graph</h1>
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: month,
+          datasets: [
+            {
+              id: 1,
+              label: "ACV",
+              backgroundColor: "blue",
+              borderColor: "blue",
+              data: acv,
+            },
+            {
+              id: 2,
+              label: "Month",
+              backgroundColor: "yellow",
+              borderColor: "green",
+
+              data: month,
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+}
+
+export default Graph;
